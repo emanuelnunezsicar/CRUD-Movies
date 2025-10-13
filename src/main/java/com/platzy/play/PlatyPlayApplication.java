c4c2a0dee29219660891810f399d15181b22388d
Emanuel Nunez <emanuel.nunez@sicar.mx>
2025-10-13 14:35:45 -0600

    init crud movies project with springboot

---
+++ src/main/java/com/platzy/play/PlatyPlayApplication.java
@@ -0,0 +1,11 @@
+package com.platzy.play;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatyPlayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PlatyPlayApplication.class, args);
+    }
+}
---
+++ src/test/java/com/platzy/play/PlatyPlayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platzy.play;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatyPlayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
